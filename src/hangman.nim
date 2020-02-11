d354f382fe5a478b923f0666e8fbc3989d9f3982
swag31415 <kidswagatam18@gmail.com>
2020-02-11 04:54:29 -0700

    implemented rudimentary hangman

--- src/hangman.nim
+++ src/hangman.nim
@@ -1,5 +1,44 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+# It's hangman!
 
-when isMainModule:
-  echo("Hello, World!")
+proc get_word(): string =
+  echo "Enter a word:"
+  return readLine(stdin)
+
+proc get_letter(): char =
+  echo "Enter a letter:"
+  return readLine(stdin)[0]
+
+proc run(word: string, solved: var seq[bool]): bool =
+  var guess = get_letter()
+  var is_correct = false;
+  for i in 0..<word.len():
+      if word[i] == guess:
+          solved[i] = true;
+          is_correct = true;
+  return is_correct
+
+proc get_is_done(solved: seq[bool]): bool =
+  var is_done = true;
+  for s in solved:
+      if not s: is_done = false
+  return is_done
+
+proc disp(word: string, solved: seq[bool]) =
+  for i in 0..<word.len():
+      if solved[i]:
+          stdout.write " ", word[i]
+      else:
+          stdout.write " _"
+  echo ""
+
+var word = get_word()
+var solved = newSeq[bool](word.len())
+while true:
+  disp(word, solved)
+  if get_is_done(solved):
+      echo "You got it!"
+      break;
+  if run(word, solved):
+      echo "You got letter(s)!"
+  else:
+      echo "oh no, no letter(s)"
